c8bd0effab8e8eff9e792231e46ebd8a710cccf2
Sunit Mishra <sunitm@contata.in>
2026-03-18 18:11:18 +0530

    kk

--- sequence.js
+++ sequence.js
@@ -6,8 +6,8 @@ async function sequence() {
             src: 'temp/1.png',
             stayTime: 5000,
             initialTransform: 'scale(1)',
-            transform: "scale(2)",
-            transition: "transform 5s ease-in",
+            transform: "scale(2) translateX(-100px)",
+            transition: "transform 10s ease-in",
         });
     }, lapsedTime);
     lapsedTime += 5000;
@@ -15,22 +15,22 @@ async function sequence() {
     setTimeout(function() {
         showImage({
             src: 'temp/2.png',
-            stayTime: 4000,
-            initialTransform: 'scale(2)',
-            transform: "scale(1)",
-            transition: "transform 4s ease-in",
+            stayTime: 7000,
+            initialTransform: 'scale(3)',
+            transform: "scale(2)",
+            transition: "transform 7s ease-in",
         });
     }, lapsedTime);
-    lapsedTime += 4000;
+    lapsedTime += 7000;
 
     setTimeout(function() {
         showImage({
             src: 'temp/3.png',
-            stayTime: 5000,
+            stayTime: 8000,
             initialTransform: 'scale(1)',
-            transform: "scale(2)",
-            transition: "transform 5s ease-in",
+            transform: "scale(3)",
+            transition: "transform 8s ease-in",
         });
     }, lapsedTime);
-    lapsedTime += 5000;
+    lapsedTime += 8000;
 }
