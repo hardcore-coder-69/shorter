d0576cb5040339ab72a6f6e6c1b5f382be6de6b6
Sunit Mishra <sunitm@contata.in>
2026-03-18 16:25:02 +0530

    fine

--- script.js
+++ script.js
@@ -22,8 +22,32 @@ async function startSequence() {
 }
 
 async function showImage(data) {
+    let imgContainerEl = document.createElement("div");
+    imgContainerEl.classList.add('single-image-container');
+    
     let imgEl = document.createElement('img');
+    imgEl.classList.add("single-image");
     imgEl.setAttribute('src', data.src);
+    imgContainerEl.appendChild(imgEl);
+    
+    setTimeout(function() {
+        imgContainerEl.classList.add("hiding-now");
+        setTimeout(() => {
+            imgContainerEl.remove();
+        }, 500);
+    }, data.stayTime - 500);
+
+    screenEl.appendChild(imgContainerEl);
+    if(data.initialTransform) {
+        imgEl.style.transform = data.initialTransform;
+        await sleep(100);
+    }
+
+    if(data.transition && data.transform) {
+        imgEl.style.transition = data.transition;
+        imgEl.style.transform = data.transform;
+    }
+    
 }
 
 async function sleep(ms) {
--- sequence.js
+++ sequence.js
@@ -4,33 +4,33 @@ async function sequence() {
     setTimeout(function() {
         showImage({
             src: 'temp/1.png',
-            stayTime: 3000,
-            initialTranform: 'scale(2)',
-            transform: "scale(1)",
-            transition: "transform 2s ease-in",
+            stayTime: 5000,
+            initialTransform: 'scale(1)',
+            transform: "scale(2)",
+            transition: "transform 5s ease-in",
         });
     }, lapsedTime);
-    lapsedTime += 3000;
+    lapsedTime += 5000;
 
     setTimeout(function() {
         showImage({
             src: 'temp/2.png',
-            stayTime: 3000,
-            initialTranform: 'scale(2)',
-            transform: "scale(1)",
-            transition: "transform 2s ease-in",
-        });
-    }, lapsedTime);
-    lapsedTime += 3000;
-
-    setTimeout(function() {
-        showImage({
-            src: 'temp/1.png',
             stayTime: 4000,
-            initialTranform: 'scale(2)',
+            initialTransform: 'scale(2)',
             transform: "scale(1)",
-            transition: "transform 2s ease-in",
+            transition: "transform 4s ease-in",
         });
     }, lapsedTime);
     lapsedTime += 4000;
+
+    setTimeout(function() {
+        showImage({
+            src: 'temp/3.png',
+            stayTime: 5000,
+            initialTransform: 'scale(1)',
+            transform: "scale(2)",
+            transition: "transform 5s ease-in",
+        });
+    }, lapsedTime);
+    lapsedTime += 5000;
 }
