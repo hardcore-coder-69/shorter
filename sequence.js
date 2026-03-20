6c6d77e6aadc9da0b157d4a1a81158741f6257ed
Sunit Mishra <sunitm@contata.in>
2026-03-20 13:49:27 +0530

    fine

--- sequence.js
+++ sequence.js
@@ -14,26 +14,62 @@ async function sequence() {
     }, lapsedTime);
     lapsedTime += 3000;
 
-    // setTimeout(function() {
-    //     showVideo({
-    //         src: 'temp/video.mp4',
-    //         playbackRate: 0.7,
-    //         stayTime: 10000,
-    //         exitSound: whooshEl,
-    //         initialTransform: 'scale(1)',
-    //         transform: "scale(1.5)",
-    //         transition: "transform 10s linear",
-    //     });
-    // }, lapsedTime);
-    // lapsedTime += 10000;
-
     setTimeout(function () {
         showImage({
             src: 'temp/2.png',
-            stayTime: 8000,
-            exitSound: pageFlipEl,
+            stayTime: 10000,
+            exitSound: fastWhooshEl,
             initialTransform: 'scale(0.5)',
             transform: "scale(1)",
+            transition: "transform 10s linear",
+        });
+    }, lapsedTime);
+    lapsedTime += 10000;
+
+    setTimeout(function () {
+        showImage({
+            src: 'temp/3.png',
+            stayTime: 4000,
+            exitSound: whooshEl,
+            initialTransform: 'scale(1)',
+            transform: "scale(1.1)",
+            transition: "transform 5s linear",
+        });
+    }, lapsedTime);
+    lapsedTime += 4000;
+
+    setTimeout(function () {
+        showImage({
+            src: 'temp/4.png',
+            stayTime: 5000,
+            exitSound: fastWhooshEl,
+            initialTransform: 'scale(1)',
+            transform: "scale(1.2)",
+            transition: "transform 7s linear",
+        });
+    }, lapsedTime);
+    lapsedTime += 5000;
+
+    setTimeout(function() {
+        showVideo({
+            src: 'temp/5.mp4',
+            playbackRate: 1,
+            stayTime: 9000,
+            exitSound: whooshEl,
+            initialTransform: 'scale(1)',
+            transform: "scale(1.5)",
+            transition: "transform 10s linear",
+        });
+    }, lapsedTime);
+    lapsedTime += 9000;
+
+    setTimeout(function () {
+        showImage({
+            src: 'temp/6.png',
+            stayTime: 10000,
+            exitSound: fastWhooshEl,
+            initialTransform: 'scale(1)',
+            transform: "scale(1.5)",
             transition: "transform 10s linear",
         });
     }, lapsedTime);
